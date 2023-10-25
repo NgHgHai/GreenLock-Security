254ce4b0bb612f30b84822747f4804c300af418f
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-10-25 15:24:52 +0700

    vigerner en-de done

---
+++ src/main/java/vn/edu/ATBMModel/symmetric/Vigener.java
@@ -0,0 +1,51 @@
+
+package vn.edu.ATBMModel.symmetric;
+
+import java.util.Arrays;
+
+public class Vigener {
+    CaesarCipher[] ciphers;
+
+    public Vigener(int[] key) {
+        ciphers = new CaesarCipher[key.length];
+        for (int i = 0; i < key.length; i++) {
+            ciphers[i] = new CaesarCipher(key[i]);
+        }
+    }
+
+    public String encrypt(String input) {
+        StringBuilder answer = new StringBuilder();
+        int i = 0;
+        for (char c : input.toCharArray()) {
+            int cipherIndex = i % ciphers.length;
+            CaesarCipher thisCipher = ciphers[cipherIndex];
+            answer.append(thisCipher.encryptLetter(c));
+            i++;
+        }
+        return answer.toString();
+    }
+
+    public String decrypt(String input) {
+        StringBuilder answer = new StringBuilder();
+        int i = 0;
+        for (char c : input.toCharArray()) {
+            int cipherIndex = i % ciphers.length;
+            CaesarCipher thisCipher = ciphers[cipherIndex];
+            answer.append(thisCipher.decryptLetter(c));
+            i++;
+        }
+        return answer.toString();
+    }
+
+    public String toString() {
+        return Arrays.toString(ciphers);
+    }
+
+    public static void main(String[] args) {
+        Vigener v = new Vigener(new int[]{17, 14, 12, 4});
+        String encrypted = v.encrypt("Hello World");
+        System.out.println(encrypted);
+        System.out.println(v.decrypt(encrypted));
+    }
+
+}
