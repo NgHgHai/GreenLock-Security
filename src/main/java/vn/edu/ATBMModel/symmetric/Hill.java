42e9c71928565e60fb3e688f0c8921eef86f1169
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-10-25 14:37:12 +0700

    hill en-de done

--- src/main/java/vn/edu/ATBMModel/symmetric/Hill.java
+++ src/main/java/vn/edu/ATBMModel/symmetric/Hill.java
@@ -1,68 +1,129 @@
 package vn.edu.ATBMModel.symmetric;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.commons.math3.linear.RealMatrix;
-
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 
 public class Hill {
-    public double[][] getKey(int n, String data) {
+    int[] vietnameseCharacters = new int[257];
+
+    public int[] initTableCharacter() {
+        vietnameseCharacters = new int[257];
+        int count = 0;
+        for (int i = 65; i <= 121; i++) {
+            vietnameseCharacters[count] = i;
+            count++;
+        }
+        for (int i = 7840; i <= 7929; i++) {
+            vietnameseCharacters[count] = i;
+            count++;
+        }
+        vietnameseCharacters[count] = 32;
+        count++;
+        for (int i = 192; i <= 300; i++) {
+            vietnameseCharacters[count] = i;
+            count++;
+        }
+        return vietnameseCharacters;
+    }
+
+    public Hill() {
+        this.vietnameseCharacters = initTableCharacter();
+    }
+
+    int modulo = vietnameseCharacters.length;
+
+    public int[][] getKey(String data) {
         StringTokenizer st = new StringTokenizer(data, " ");
-        double[][] key = new double[n][n];
-        if (st.countTokens() != n * n) {
+        int[][] key = new int[2][2];
+        if (st.countTokens() != 2 * 2) {
             return null;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                key[i][j] = Double.parseDouble(st.nextToken());
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                key[i][j] = getNum(st.nextToken());
             }
         }
         return key;
     }
 
-    public double[][] encrypt(double[][] key, double[][] messVector) {
-        RealMatrix keyMatrix = new Array2DRowRealMatrix(key);
-        RealMatrix messMatrix = new Array2DRowRealMatrix(messVector);
-        RealMatrix cipherMatrix = keyMatrix.multiply(messMatrix);
-        return cipherMatrix.getData();
-    }
-
-    public double[][] decrypt(double[][] key, double[][] cipherVector) {
-        RealMatrix keyMatrix = new Array2DRowRealMatrix(key);
-        RealMatrix cipherMatrix = new Array2DRowRealMatrix(cipherVector);
-        RealMatrix inverseKeyMatrix = MatrixUtils.inverse(keyMatrix);
-        RealMatrix messMatrix = inverseKeyMatrix.multiply(cipherMatrix);
-        return messMatrix.getData();
-    }
-
-    public String hillCipherEnc(String key, String mess, int n) {
-        String result = "";
-        int count = 0;
-        double[][] messVector = new double[n][1];
-        while (count <= mess.length()) {
-            for (int i = 0; i < n; i++) {
-                try {
-                    messVector[i][0] = mess.charAt(i + count) - 65;
-                } catch (Exception e) {
-                    messVector[i][0] = 0;
-//                messVector[i][0] = mess.charAt(i + count) - 65;
-                }
-            }
-            count = count + n;
-            double[][] enc = encrypt(getKey(n, key), messVector);
-            for (int i = 0; i < enc.length; i++) {
-                System.out.println(enc[i][0] % 26);
-                result += (char) ((int) (enc[i][0] % 26) + 65);
-                System.out.println(result);
+    private int getNum(String s) {
+        for (int i = 0; i < vietnameseCharacters.length; i++) {
+            if (s.codePointAt(0) == vietnameseCharacters[i]) {
+                return i;
             }
         }
-        return result;
+        return 148;
+    }
+
+    private static int[][] reverseMatrix(int[][] keyMatrix, int modulo) {
+        int detmod68 = (keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[0][1] * keyMatrix[1][0] + modulo) % modulo; // Calculate the determinant modulo 68
+        int factor;
+
+        // Find the factor for which is true that
+        // factor * det = 1 mod modulo
+        for (factor = 1; factor < modulo; factor++) {
+            if ((detmod68 * factor) % modulo == 1) {
+                break;
+            }
+        }
+
+        int[][] reverseMatrix = new int[2][2];
+
+        // Calculate the reverse key matrix elements using the factor found
+        reverseMatrix[0][0] = (keyMatrix[1][1] * factor) % modulo;
+        reverseMatrix[0][1] = ((modulo - keyMatrix[0][1]) * factor) % modulo;
+        reverseMatrix[1][0] = ((modulo - keyMatrix[1][0]) * factor) % modulo;
+        reverseMatrix[1][1] = (keyMatrix[0][0] * factor) % modulo;
+
+        return reverseMatrix;
+    }
+
+    public String encrypt(int[][] key, String mess) {
+        if (mess.length() % 2 == 1) {
+            mess += " ";
+        }
+        ArrayList<Integer> messlist = new ArrayList<>();
+        ArrayList<Character> result = new ArrayList<>();
+        for (int i = 0; i < mess.length(); i++) {
+            messlist.add(getNum(mess.charAt(i) + ""));
+        }
+        for (int i = 0; i < messlist.size(); i += 2) {
+
+            int x = (key[0][0] * messlist.get(i) + key[0][1] * messlist.get(i + 1)) % modulo;
+            int y = (key[1][0] * messlist.get(i) + key[1][1] * messlist.get(i + 1)) % modulo;
+            result.add((char) vietnameseCharacters[x]);
+            result.add((char) vietnameseCharacters[y]);
+        }
+        return printResult(result);
+    }
+
+    public String decrypt(int[][] key, String mess) {
+        ArrayList<Integer> messlist = new ArrayList<>();
+        ArrayList<Character> result = new ArrayList<>();
+        for (int i = 0; i < mess.length(); i++) {
+            messlist.add(getNum(mess.charAt(i) + ""));
+        }
+        int[][] reverseKey = reverseMatrix(key, modulo);
+        for (int i = 0; i < messlist.size(); i += 2) {
+            int x = (reverseKey[0][0] * messlist.get(i) + reverseKey[0][1] * messlist.get(i + 1)) % modulo;
+            int y = (reverseKey[1][0] * messlist.get(i) + reverseKey[1][1] * messlist.get(i + 1)) % modulo;
+            result.add((char) vietnameseCharacters[x]);
+            result.add((char) vietnameseCharacters[y]);
+        }
+        return printResult(result);
+    }
+
+    private String printResult(ArrayList<Character> result) {
+        String s = "";
+        for (int i = 0; i < result.size(); i++) {
+            s += result.get(i);
+        }
+        return s;
     }
 
 
-    public void printMatrix(double[][] matrix) {
+    public void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             System.out.print("|");
             for (int j = 0; j < matrix.length; j++) {
@@ -73,47 +134,24 @@ public class Hill {
         }
     }
 
+    public void printUnicode() {
+        System.out.println(vietnameseCharacters.length);
+        for (int i = 0; i < vietnameseCharacters.length; i++) {
+            System.out.print(vietnameseCharacters[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        String data = "6 24 1 13 16 10 20 17 15";
-        double[][] key = new double[3][3];
         Hill hill = new Hill();
-        key = hill.getKey(3, data);
-        hill.printMatrix(key);
-        String mess = "ACTACTACTA";
-        System.out.println(hill.hillCipherEnc(data, mess, 3));
 
-//        System.out.println('a'- 0);
-
-
-//        double[][] key = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
-//        double[][] plaintext = {{0}, {2}, {19}};
-//        int[][] ciphertext = new int[3][1];
-//        int[][] plaintext1 = new int[3][1];
-//        RealMatrix keyMatrix = new Array2DRowRealMatrix(key);
-//
-//        RealMatrix inverseKeyMatrix = MatrixUtils.inverse(keyMatrix);
-//
-//        RealMatrix plaintextMatrix = new Array2DRowRealMatrix(plaintext);
-//
-//        RealMatrix ciphertextMatrix = keyMatrix.multiply(plaintextMatrix);
-//
-//        ciphertext = new int[ciphertextMatrix.getRowDimension()][ciphertextMatrix.getColumnDimension()];
-//
-//        for (int i = 0; i < ciphertextMatrix.getRowDimension(); i++) {
-//            for (int j = 0; j < ciphertextMatrix.getColumnDimension(); j++) {
-//                ciphertext[i][j] = (int) ciphertextMatrix.getEntry(i, j);
-//                System.out.print(ciphertext[i][j] % 26 + " ");
-//            }
-//            System.out.println();
+        int[][] key = {{16, 33}, {11, 15}};
+        String hillenc = hill.encrypt(key, "okokokokokokokokok");
+        System.out.println(hillenc);
+        System.out.println(hill.decrypt(key, hillenc));
+//        System.out.println("ỹ".codePointAt(0)== (char)7929);
+//        for (int i = 192; i<=400;i++){
+//            System.out.println((char) i);
 //        }
-//        RealMatrix plaintextMatrix1 = inverseKeyMatrix.multiply(ciphertextMatrix);
-//        plaintext1 = new int[plaintextMatrix1.getRowDimension()][plaintextMatrix1.getColumnDimension()];
-//        for (int i = 0; i < plaintextMatrix1.getRowDimension(); i++) {
-//            for (int j = 0; j < plaintextMatrix1.getColumnDimension(); j++) {
-//                plaintext1[i][j] = (int) plaintextMatrix1.getEntry(i, j);
-//                System.out.print(plaintext1[i][j] % 26 + " ");
-//            }
-//            System.out.println();
-//        }
+//        System.out.println("Ý".codePointAt(0));
     }
 }
